d1c671d5af184bc961e69f3e983f1454376e48d4
sagi <asagip8@hotmail.com>
2015-06-19 11:28:08 +0300

    Web Api added

---
+++ DesktopClient/DesktopClient/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNet.SignalR.Client.Connection;
+
+namespace DesktopClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var hubConnection = new HubConnection("http://www.contoso.com/");
+            IHubProxy stockTickerHubProxy = hubConnection.CreateHubProxy("StockTickerHub");
+            stockTickerHubProxy.On<Stock>("UpdateStockPrice", stock => Console.WriteLine("Stock update for {0} new price {1}", stock.Symbol, stock.Price));
+            await hubConnection.Start();
+
+        }
+    }
+}
