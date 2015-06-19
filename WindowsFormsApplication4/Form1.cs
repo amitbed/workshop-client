c937f9e5f2d627e45a851e0ac8e6a41805ab4990
Amit Bedarshi <amitbed@post.bgu.ac.il>
2015-06-19 18:47:19 +0300

    new project

---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://Amit-PC:49417/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            try
+            {
+                HttpResponseMessage resp = client.GetAsync("api/Values").Result;
+                resp.EnsureSuccessStatusCode();  // Throw exception if not a success code.
+                label1.Text = "";
+                label1.Text = resp.Content.ReadAsAsync<IEnumerable<string>>().Result.First();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+        }
+    }
+}
