13df3bc1f81c4eb516a4cf4fe2a910dc0cdf0630
sagi <asagip8@hotmail.com>
2015-06-23 10:15:49 +0300

    Client

--- WindowsFormsApplication4/RegisterForm.cs
+++ WindowsFormsApplication4/RegisterForm.cs
@@ -48,7 +48,7 @@ namespace WindowsFormsApplication4
                 usernameToolStripMenuItem.Text = usernameToolStripMenuItem.Text.ToString() + hpf.getUsername();
 
                 HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri("http://localhost:49417/");
+                client.BaseAddress = new Uri("http://amit-pc:49417/");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 try
@@ -76,7 +76,7 @@ namespace WindowsFormsApplication4
                 args.Add(EmailTextBox.Text.ToString());
 
                 HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri("http://localhost:49417/");
+                client.BaseAddress = new Uri("http://amit-pc:49417/");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 try
